c24155bd5dcec912711746247e5c026aa32651df
ThDat1301 <2051052028dat@ou.edu.vn>
2022-11-07 19:16:25 +0700

    fix conflict

--- app/index.py
+++ app/index.py
@@ -7,16 +7,14 @@ def home():
     return render_template('index.html')
 
 
-<<<<<<< HEAD
 @app.route("/dang-ky-kham-truc-tuyen")
 def make_appointment():
     return render_template('appointment.html')
 
-=======
 @app.route("/login")
 def login():
     return render_template('login.html')
->>>>>>> 9496dad1edaf1a740e9bba089a8ab7fe45fbc278
+
 
 if __name__ == '__main__':
     app.run(debug=True)
